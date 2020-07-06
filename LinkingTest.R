da17f88925b6eb2744d15ac7a97201837e489bb1
Swetha Ravichandran <swetha.ravichandran96@gmail.com>
2020-07-06 20:11:41 +0530

    Noted the steps to link GitHub with RStudio for future reference!

---
+++ LinkingTest.R
@@ -0,0 +1,7 @@
+print("STEPS TO linking Git with RStudio")
+
+print("Go to GitHub and create a new repository. Copy the Git link from there.")
+print("Then go to RStudio and go to File > New Project > Version Control > paste the copied Git link")
+print("In RStudio, the project opens. Now create File > New File > R Script")
+print(" Now save the project and in your Git quadrant-stage it, commit it and Push it.")
+print("Now you are done!")
